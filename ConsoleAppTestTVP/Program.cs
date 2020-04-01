c5d58e5122dbbeb8457284f57977ce3c9e24a875
Ramzy Arviano <arviano.ramzy@gmail.com>
2020-04-01 22:58:50 +0700

    success run

--- ConsoleAppTestTVP/Program.cs
+++ ConsoleAppTestTVP/Program.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace ConsoleAppTestTVP
 {
@@ -9,12 +11,68 @@ namespace ConsoleAppTestTVP
 	{
 		static void Main(string[] args)
 		{
-			// The code provided will print ‘Hello World’ to the console.
-			// Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
 			Console.WriteLine("Hello World!");
+
+			IEnumerable<long> ListId = new List<long>
+			{
+				4,
+				3,
+				2,
+				1
+			};
+
+			ReleaseProjectHold(ListId);
+
 			Console.ReadKey();
 
-			// Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+		}
+
+		public static void ReleaseProjectHold(IEnumerable<long> ListId)
+		{
+			//Checking sql
+			string conString = "Data Source=DESKTOP-BTC8QL7;initial catalog=Jajal;Trusted_Connection=True;";
+			try
+			{
+				using (SqlConnection con = new SqlConnection(conString))
+				{
+					con.Open();
+					using (SqlCommand cmd = new SqlCommand("[dbo].[sp_jajal_TVP]", con))
+					{
+						cmd.CommandType = CommandType.StoredProcedure;
+						SqlParameter tvpParam = cmd.Parameters.AddWithValue("@InsertVar_TVP", CreateDataTable(ListId));
+						tvpParam.SqlDbType = SqlDbType.Structured;
+						tvpParam.TypeName = "dbo.Var_TVP";
+
+						using (SqlDataReader dr = cmd.ExecuteReader())
+						{
+							if (dr.HasRows)
+							{
+								while (dr.Read())
+								{
+									Console.WriteLine(dr["ID"].ToString() + " " + dr["StringAdd"].ToString());
+								}
+							}
+						}
+
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.StackTrace);
+			}
+
+		}
+
+		private static DataTable CreateDataTable(IEnumerable<long> ids)
+		{
+			DataTable table = new DataTable();
+			table.Columns.Add("ID", typeof(long));
+			foreach (long id in ids)
+			{
+				table.Rows.Add(id);
+			}
+			return table;
 		}
 	}
 }
